e17a7ff10d2c8ccd3d53f6c7bda9cab24d443e5b
Jason Chen <chenjason971@gmail.com>
2022-06-13 11:46:35 -0500

    Finished Project

--- Math Operators/Program.cs
+++ Math Operators/Program.cs
@@ -10,6 +10,23 @@ namespace Math_Operators
     {
         static void Main(string[] args)
         {
+            //Asks user for input
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Prints the ouput of adding, subtracting, multiplying, and dividing 
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+
+            Console.ReadKey();
+            
+
+
         }
     }
 }
